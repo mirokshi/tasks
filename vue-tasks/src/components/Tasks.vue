8262c16a7589594823f99ee6f57741cf5b354e5b
mirokshi <rojasdiazmirokshi@gmail.com>
2018-10-03 18:02:08 +0200

    WIP

---
+++ vue-tasks/src/components/Tasks.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li>Task1</li>
+        <li>Task2</li>
+        <li>Task3</li>
+        <li>Task4</li>
+        <li>Task5</li>
+    </ul>
+</template>
+
+<script>
+
+</script>
+
+<styles>
+
+</styles>
