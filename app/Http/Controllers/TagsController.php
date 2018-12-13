2d7eb952742f190a0d5fcdfcff20ee292b349303
mirokshi <rojasdiazmirokshi@gmail.com>
2018-12-13 21:30:20 +0100

    Sync

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 
 use App\Http\Requests\TagIndex;
-use App\Http\Requests\UserTagsIndex;
+
 use App\Tag;
 use App\User;
 use Illuminate\Support\Facades\Auth;
@@ -12,19 +12,12 @@ use Illuminate\Support\Facades\Auth;
 class TagsController extends Controller
 {
 
-    public function index(UserTagsIndex $request)
+    public function index(TagIndex $request)
     {
-        $tags =map_collection(Tag::all());
-        if (Auth::user()->can('tags.manage')){
-            $tags = map_collection(Tag::orderBy('created_at','desc') -> get());
-            $uri = 'api/v1/tag/';
-        }else {
-            $tags = map_collection($request->user()->tasks);
-            $uri = 'api/v1/user/tag/';
-        }
-        // MVC
-        $users= User::all();
-        return view('tags',compact('tags', 'uri'));
+        $tags = map_collection(Tag::all());
+        return view('tags',[
+            'tags' => $tags
+        ]);
     }
 
 }
