2a574289bcb9aa81fc87b8d988b5e87b2d2daa26
mirokshi <rojasdiazmirokshi@gmail.com>
2019-01-18 19:14:50 +0100

    Sync

--- tests/Unit/MailTest.php
+++ tests/Unit/MailTest.php
@@ -11,51 +11,49 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
-//class MailTest extends TestCase{
-  //  use RefreshDatabase;
+class MailTest extends TestCase{
+    use RefreshDatabase;
 
-//    /**
-//     * @test
-//     */
-//    public function send_email()
-//    {
-//
-//        dump(env('MAIL_DRIVER'));
-//        //1
-//        $user = factory(User::class)->create();
-//        //2
-//        Mail::to($user)->send(new TestMail());
-//        //3
-//        $this->assertTrue(true);
-//    }
-//
-//    /**
-//     * @test
-//     */
-//    public function send_markdown_dinamic_email()
-//    {
-//
-//        dump(env('MAIL_DRIVER'));
-//        //1
-//        $user = factory(User::class)->create();
-//        //2
-//        Mail::to($user)->send(new TestDinamicEmail($user));
-//        //3
-//        $this->assertTrue(true);
-//    }
-//
-//    /**
-//     * @test
-//     */
-//    public function send_welcome_email()
-//    {
-//        dump(env('MAIL_DRIVER'));
-//        //1
-//        $user = factory(User::class)->create();
-//        //2
-//        Mail::to($user)->send(new WelcomeEmail($user));
-//        //3
-//        $this->assertTrue(true);
-//
-//    }
-//}
+    /**
+     * @test
+     */
+    public function send_email()
+    {
+
+        //1
+        $user = factory(User::class)->create();
+        //2
+        Mail::to($user)->send(new TestMail());
+        //3
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     */
+    public function send_markdown_dinamic_email()
+    {
+
+        //1
+        $user = factory(User::class)->create();
+        //2
+        Mail::to($user)->send(new TestDinamicEmail($user));
+        //3
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     */
+    public function send_welcome_email()
+    {
+
+        //1
+        $user = factory(User::class)->create();
+        //2
+        Mail::to($user)->send(new WelcomeEmail($user));
+        //3
+        $this->assertTrue(true);
+
+    }
+}
