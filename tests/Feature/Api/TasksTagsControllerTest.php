2696d51c158f51d376d1a71cde09fd99b3815b94
mirokshi <rojasdiazmirokshi@gmail.com>
2019-01-23 18:36:18 +0100

    Sync

--- tests/Feature/Api/TasksTagsControllerTest.php
+++ tests/Feature/Api/TasksTagsControllerTest.php
@@ -29,7 +29,7 @@ class TasksTagsControllerTest extends TestCase{
         ]);
 
         //2
-        $response = $this->json('PUT','/api/v1/tasks/'.$task->id.'/tags/',[
+        $response = $this->json('PUT','/api/v1/tasks/' . $task->id . '/tags/',[
             'tags'=>[$tag->id]
         ]);
 
@@ -41,36 +41,75 @@ class TasksTagsControllerTest extends TestCase{
         $this->assertCount(1,$task->tags);
         $this->assertEquals('home',$task->tags[0]->name);
         $this->assertTrue($task->tags[0]->is($tag));
+        $this->assertEquals($task->tags[0]->id, $tag->id);
     }
 
     /**
      * @test
      */
-    public function guest_user_cannoot_add_tag_to_tasks()
+    public function can_add_tag_to_task_validation()
     {
-        //1 Prepare
+        $this->loginAsTaskManager('api');
+        // 1 Prepare
         $task = Task::create([
-            'name' => 'Comprar pan'
-        ]);
-        $tag = Tag::create ([
-            'name' => 'home'
+            'name' => 'Comprar pa'
         ]);
 
-        //2
-        $response = $this->json('PUT','/api/v1/tasks'.$task->id.'/tags/',[
-            'tags'=>[$tag->id]
+        $tag = Tag::create([
+            'name' => 'home',
+            'description' => 'bla bla',
+            'color' => 'blue'
         ]);
+        $this->assertCount(0,$task->tags);
+        $response = $this->json('PUT','/api/v1/tasks/' . $task->id . '/tags/');
 
-        //3
-        $response->assertSuccessful();
-
-        $task = $task->fresh();
-
-        $this->assertCount(1,$task->tags);
-        $this->assertEquals('home',$task->tags[0]->name);
-        $this->assertTrue($task->tags[0]->is($tag));
-
-
+        $response->assertStatus(422);
     }
 
+    /**
+     * @test
+     */
+    public function regular_user_cannot_add_tag_to_task()
+    {
+        $this->login('api');
+        // 1 Prepare
+        $task = Task::create([
+            'name' => 'Comprar pa'
+        ]);
+
+        $tag = Tag::create([
+            'name' => 'home',
+            'description' => 'bla bla',
+            'color' => 'blue'
+        ]);
+        $this->assertCount(0,$task->tags);
+        $response = $this->json('PUT','/api/v1/tasks/' . $task->id . '/tags/', [
+            'tags' => [$tag->id]
+        ]);
+
+        $response->assertStatus(403);
+    }
+
+    /**
+     * @test
+     */
+    public function guest_user_cannot_add_tag_to_task()
+    {
+        // 1 Prepare
+        $task = Task::create([
+            'name' => 'Comprar pa'
+        ]);
+
+        $tag = Tag::create([
+            'name' => 'home',
+            'description' => 'bla bla',
+            'color' => 'blue'
+        ]);
+        $this->assertCount(0,$task->tags);
+        $response = $this->json('PUT','/api/v1/tasks/' . $task->id . '/tags/', [
+            'tags' => [$tag->id]
+        ]);
+
+        $response->assertStatus(401);
+    }
 }
