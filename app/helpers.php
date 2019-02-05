7af7ce68eceece6d8b10381c5b2da877aef1d52c
mirokshi <rojasdiazmirokshi@gmail.com>
2019-02-05 20:56:13 +0100

    Sync

--- app/helpers.php
+++ app/helpers.php
@@ -614,13 +614,11 @@ if (! function_exists('sample_logs')) {
          */
         function is_valid_uuid($uuid)
         {
-
             if (!is_string($uuid) || (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid) !== 1)) {
                 return false;
             }
             return true;
         }
-
     }
 
     if (! function_exists('set_sample_notifications_to_user')) {
