5e8370d003e35d874ddab80cf80cc01b411460f7
mirokshi <rojasdiazmirokshi@gmail.com>
2019-03-05 19:38:49 +0100

    Sync

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-importScripts("/service-worker/precache-manifest.75a28d47cbe56bf22f501b64be5669be.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/service-worker/precache-manifest.0f61ab87000baa688d43564a6ac270ce.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.setConfig({
   debug: true
